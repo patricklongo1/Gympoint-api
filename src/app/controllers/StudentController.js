df4cea38375b0384fddcfba82f2204253d01ca1b
Patrick Longo <ck.longo@bol.com.br>
2019-10-23 10:52:39 -0300

    Auth done

---
+++ src/app/controllers/StudentController.js
@@ -0,0 +1,71 @@
+import * as Yup from 'yup';
+
+import Student from '../models/Student';
+
+class StudentController {
+    async store(req, res) {
+        const schema = Yup.object().shape({
+            name: Yup.string().required(),
+            email: Yup.string()
+                .email()
+                .required(),
+            age: Yup.number().required(),
+            weight: Yup.number().required(),
+            height: Yup.number().required(),
+        });
+
+        if (!(await schema.isValid(req.body))) {
+            return res.status(400).json({ error: 'Validation fails' });
+        }
+
+        const studentExists = await Student.findOne({
+            where: { email: req.body.email },
+        });
+        if (studentExists) {
+            return res.status(400).json({ error: 'Student already exists' });
+        }
+
+        const { id, name, email } = await Student.create(req.body); // substitui const {user} para não retornar tudo.
+
+        return res.json({
+            id,
+            name,
+            email,
+        });
+    }
+
+    async update(req, res) {
+        const schema = Yup.object().shape({
+            name: Yup.string(),
+            email: Yup.string().email(),
+            weight: Yup.number(),
+            height: Yup.number(),
+        });
+
+        if (!(await schema.isValid(req.body))) {
+            return res.status(400).json({ error: 'Validation fails' });
+        }
+        // console.log(req.userId) utiliza o id do usuario pegando do token de auth, criado em middlewares/auth.js. neste caso
+        // não é preciso pegar o id do user por parametro antes de atualizar seus dados, pois este id ja consta no token.
+        const { email } = req.body;
+        const student = await Student.findByPk(req.userId);
+        if (email !== student.email) {
+            const studentExists = await Student.findOne({ where: { email } });
+
+            if (studentExists) {
+                return res
+                    .status(400)
+                    .json({ error: 'Student already exists' });
+            }
+        }
+
+        const { id, name } = await student.update(req.body);
+        return res.json({
+            id,
+            name,
+            email,
+        });
+    }
+}
+
+export default new StudentController();
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,15 @@
 import { Router } from 'express';
 
+import SessionController from './app/controllers/SessionController';
+import StudentController from './app/controllers/StudentController';
+
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-    return res.json({ message: 'Testing..' });
-});
+routes.post('/', SessionController.store);
+
+routes.use(authMiddleware); // Só passa pelo middleware o que vem abaixo.
+routes.post('/students', StudentController.store);
 
 export default routes;
